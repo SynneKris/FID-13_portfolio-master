d57bd96de17bc35d04db037771e7a39a35f0b3e9
SynneKris <74900354+SynneKris@users.noreply.github.com>
2022-05-20 10:04:54 +0200

    first commit

---
+++ js/env.js
@@ -0,0 +1,5 @@
+// her definer noen generisk variabler: den første er cdn (content distribution network)
+// her inne sanity lagre bilder vi lastet opp i sanity platform
+export const projectID = '14m8q60u'
+export const cdnUrl = `https://cdn.sanity.io/images/14m8q60u/production/`;
+
